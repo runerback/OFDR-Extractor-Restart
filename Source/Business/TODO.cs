026ebbbea52d245402feb042b787c516abb842f2
ruenrback <runerback@hotmail.com>
2018-06-03 21:15:36 +0800

    2

---
+++ Source/Business/TODO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OFDRExtractor.Business
+{
+	//handle folders first, extract files and move to container folder after all folders are in position.
+	//get about 5 front folder name order by index, then compare with folder tree built from ProcessMonitor result.
+	//calculated folder tree should same as before.
+}
